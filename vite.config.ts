14103ce5beb7612c0d1afa63a55a15b183566544
Ali Mahdiyanjoo <65537987+Ali-Mahdiyanjoo@users.noreply.github.com>
2026-06-10 14:38:32 +0200

    Change base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/safe-team-publish.github.io/',
+  base: '/esn-nancy.github.io/safe-team-publish.github.io/',
 });
